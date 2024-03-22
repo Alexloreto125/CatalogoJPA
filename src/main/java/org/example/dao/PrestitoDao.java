af5a2ed13e21fe11a515fa71bd5ec212c42258e8
Alexloreto125 <alexloreto125@gmail.com>
2024-03-22 15:47:05 +0100

    AddPrestito

--- src/main/java/org/example/dao/PrestitoDao.java
+++ src/main/java/org/example/dao/PrestitoDao.java
@@ -2,8 +2,12 @@ package org.example.dao;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.TypedQuery;
 import org.example.entities.Prestito;
 import org.example.exceptions.NotFoundExceptionPrestito;
+
+import java.util.List;
+import java.util.UUID;
 
 public class PrestitoDao {
 
@@ -16,17 +20,28 @@ public class PrestitoDao {
 
     public void savePrestito(Prestito prestito){
         try {
-            EntityTransaction transaction = em.getTransaction();
-
-            transaction.begin();
-
-            em.persist(prestito);
-            transaction.commit();
 
 
-            System.out.println("Prestito salvato");
 
-            System.out.println("ID DEL PRESTITO: "+ prestito.getId_prestito());
+            if ( !checkDisp(prestito.getIsbn())){
+                System.out.println(" Libro non disponibile ");
+
+            }
+            else {
+
+                EntityTransaction transaction = em.getTransaction();
+
+                transaction.begin();
+
+                em.persist(prestito);
+                transaction.commit();
+
+
+                System.out.println("Prestito salvato");
+
+                System.out.println("ID DEL PRESTITO: "+ prestito.getId_prestito());
+
+            }
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
@@ -35,6 +50,7 @@ public class PrestitoDao {
     public Prestito findPrestito(long id_prestito){
 
         Prestito prestito= em.find(Prestito.class, id_prestito);
+
         if(prestito == null) {
             throw new NotFoundExceptionPrestito(id_prestito);
         }
@@ -44,4 +60,25 @@ public class PrestitoDao {
 
     }
 
+    public boolean checkDisp(String isbn){
+        TypedQuery<Prestito> queryDisp = em.createQuery("SELECT d FROM Prestito d WHERE d.isbn = :isbn AND d.dataRestituzioneEffettiva IS NULL", Prestito.class);
+        queryDisp.setParameter("isbn", isbn);
+        List<Prestito> prestitiNonRestituiti = queryDisp.getResultList();
+        return prestitiNonRestituiti.isEmpty();
+    }
+
+    public List<Prestito> checkElementiInPrestito(String tessera){
+        TypedQuery<Prestito> queryDisp = em.createQuery("SELECT d FROM Prestito d WHERE d.tessera = :tessera AND d.dataRestituzioneEffettiva IS NULL", Prestito.class);
+        queryDisp.setParameter("tessera", tessera);
+        List<Prestito> elementiInPrestito = queryDisp.getResultList();
+        System.out.println("ELEMENTO IN PRESTITO : "+ elementiInPrestito);
+        return elementiInPrestito ;
+    }
+
+
+    public List<Prestito> elementiScaduti(){
+        TypedQuery<Prestito> queryDisp = em.createQuery("SELECT d FROM Prestito d WHERE d.dataRestituzioneEffettiva IS NULL", Prestito.class);
+        List<Prestito> elementiDaRestituire = queryDisp.getResultList();
+        return elementiDaRestituire ;
+    }
 }
